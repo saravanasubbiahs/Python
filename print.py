e98fe699efc7c9c4b502125d8860b0d0266b820d
saravanasubbiahs <saravanasubbiahs01@gmail.com>
2020-12-01 01:30:20 +0530

    updated the code

--- print.py
+++ print.py
@@ -21,3 +21,15 @@ print("Double quotes")
 ### Getting input from user
 name = input("Enter your name ")
 print("Hello " + name)
+
+# to use single quotes within print statement we have to use escape sequences \
+print('This is saravana\'s friend')
+
+# we can use print statement to even do mathematical calculations.
+print(1+2)
+
+# here we have assigned the values to the variables and then calculated and stored in variable c and atlast printed the value.
+a = 1
+b = 2
+c = a + b
+print(c)
