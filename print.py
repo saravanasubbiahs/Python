f1cb49cadade5535014fc01194b36db41626187a
saravanasubbiahs <saravanasubbiahs01@gmail.com>
2020-12-01 00:23:48 +0530

    added codes.

--- print.py
+++ print.py
@@ -1,10 +1,15 @@
 # Python
+
 ## High level Programming language(Human readable)
 ## created by Guido Van Rossum.
 ## Multi paradigm language
 ##    -> It supports object oriented programming , Structured programming , functional programming and aspect-oriented programming
 ## It have a wonderful community.
 
+## Usages
+
+###  In Machine learning and Deep Learning(AI).
+###  In web applications, GUI, and also in games.
 
 ## printing Hello world program
 print('Hello World')
