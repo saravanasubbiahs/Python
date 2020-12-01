81632042d45f7b2141c4c19a7feecdee57463cc2
saravanasubbiahs <saravanasubbiahs01@gmail.com>
2020-12-02 03:37:24 +0530

    updated py file.

--- print.py
+++ print.py
@@ -5,7 +5,7 @@
 ## Multi paradigm language
 ##    -> It supports object oriented programming , Structured programming , functional programming and aspect-oriented programming
 ## It have a wonderful community.
-
+## and have .py as their extension.
 ## Usages
 
 ###  In Machine learning and Deep Learning(AI).
