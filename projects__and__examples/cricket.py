84769deb0a1663ac31c3f36ad3a286ab0ab548ca
saravanasubbiahs <saravanasubbiahs01@gmail.com>
2020-12-21 19:57:28 +0530

    updated cricket.py

---
+++ projects__and__examples/cricket.py
@@ -0,0 +1,48 @@
+import random
+import sys
+
+out = True
+
+while out == True:
+    toss = input("Enter you choice between batting or bowling \n")
+
+    computer = random.randint(1,5)
+
+    if toss == 'batting':   
+        cho = True
+        score = 0
+        while cho == True:
+            print("Enter numbers between 1 to 5")
+            inp = input()
+            try:
+                if int(inp) < 6:
+                    if int(inp) == computer:
+                        print("you are Out with a score of " + str(score) + ' runs')
+                        break
+                    else:
+                        score = score + int(inp)
+                else:
+                    print("Enter a valid number between 1 to 5")
+                     
+            except ValueError:
+                print("Enter a valid number between 1 to 5")
+                cho = True
+    else:
+        print("start bowling")
+        print("Enter numbers between 1 to 5")
+        bowl = True
+        run = 0
+
+        while bowl == True:
+                    
+            man_put = input()
+            rand_bowl = random.randint(1,5)
+            
+            if rand_bowl == int(man_put):
+                
+                print("you have to score " + str(run + 1)+ " to win")
+                break 
+
+            else:
+                run = run + rand_bowl
+                print("run till now", run)
